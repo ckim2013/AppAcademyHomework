26a2fcd2503cee21e13bec2ddeef0b2eb17ab703
chrisykim2013@gmail.com <chrisykim2013@gmail.com>
2017-08-03 22:19:04 -0700

    Modify own methods to match more closely with solutions and add show and recursive deep_dup

--- W1D5/diy_adts.rb
+++ W1D5/diy_adts.rb
@@ -6,15 +6,15 @@ class Stack
 
   def add(el)
     @stack.push(el)
+    el
   end
 
   def remove
     @stack.pop
-    @stack
   end
 
   def show
-    @stack
+    @stack.dup
   end
 end
 
@@ -25,16 +25,16 @@ class Queue
   end
 
   def enqueue(el)
-    @queue.unshift(el)
+    @queue.push(el)
+    el
   end
 
   def dequeue
-    @queue.pop
-    @queue
+    @queue.shift
   end
 
   def show
-    @queue
+    @queue.dup
   end
 end
 
@@ -51,90 +51,101 @@ class Map
     else
       @my_map << [key, value]
     end
+    [key, value]
   end
 
   def lookup(key)
-    @my_map.select { |k, v| k == key }.flatten
+    @my_map.each { |pair| return pair if pair[0] == key }
+    nil
   end
 
   def remove(key)
     @my_map.reject! { |k, v| k == key }
   end
+
+  def show
+    deep_dup(@my_map)
+  end
+end
+
+private
+def deep_dup(array)
+  array.inject([]) { |result, el| result << (el.is_a?(Array) ? deep_dup(el) : el) }
 end
 
 # a/A solutions below!
-# EXERCISE 1
-class Stack
-  def initialize
-    @stack = []
-  end
-
-  def add(el)
-    @stack << el
-    el
-  end
-
-  def remove
-    @stack.pop
-  end
-
-  def show
-    @stack.dup
-  end
-end
-
-
-
-# EXERCISE 2
-class Queue
-  def initialize
-    @queue = []
-  end
-
-  def enqueue(el)
-    @queue << el
-    el
-  end
-
-  def dequeue
-    @queue.shift
-  end
-
-  def show
-    @queue.dup
-  end
-end
-
-
-
-# EXERCISE 3
-class Map
-  def initialize
-    @map = []
-  end
-
-  def assign(key, value)
-    pair_index = @map.index {|pair| pair[0] == key}
-    pair_index ? @map[pair_index][1] = value : @map.push([key, value])
-    [key, value]
-  end
-
-  def lookup(key)
-    @map.each {|pair| return pair[1] if pair[0] == key}
-    nil
-  end
-
-  def remove(key)
-    @map.reject! {|pair| pair[0] == key}
-    nil
-  end
-
-  def show
-    deep_dup(@map)
-  end
-
-  private
-  def deep_dup(arr)
-    arr.map { |el| el.is_a?(Array) ? deep_dup(el) : el }
-  end
-end
+# # EXERCISE 1
+# class Stack
+#   def initialize
+#     @stack = []
+#   end
+#
+#   def add(el)
+#     @stack << el
+#     el
+#   end
+#
+#   def remove
+#     @stack.pop
+#   end
+#
+#   def show
+#     @stack.dup
+#   end
+# end
+#
+#
+#
+# # EXERCISE 2
+# class Queue
+#   def initialize
+#     @queue = []
+#   end
+#
+#   def enqueue(el)
+#     @queue << el
+#     el
+#   end
+#
+#   def dequeue
+#     @queue.shift
+#   end
+#
+#   def show
+#     @queue.dup
+#   end
+# end
+#
+#
+#
+# # EXERCISE 3
+# class Map
+#   def initialize
+#     @map = []
+#   end
+#
+#   def assign(key, value)
+#     pair_index = @map.index {|pair| pair[0] == key}
+#     pair_index ? @map[pair_index][1] = value : @map.push([key, value])
+#     [key, value]
+#   end
+#
+#   def lookup(key)
+#     @map.each {|pair| return pair[1] if pair[0] == key}
+#     nil
+#   end
+#
+#   def remove(key)
+#     @map.reject! {|pair| pair[0] == key}
+#     nil
+#   end
+#
+#   def show
+#     deep_dup(@map)
+#   end
+#
+#   private
+#   def deep_dup(arr)
+#     arr.map { |el| el.is_a?(Array) ? deep_dup(el) : el }
+#   end
+# end
